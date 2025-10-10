7c13569fa6aa6262acc5d16bd1fcb97b15f43337
Irene Magozzi <irenemagozzi@gmail.com>
2025-10-10 01:12:55 -0300

    Un hechicero lo hizo

--- src/config/environment.config.js
+++ src/config/environment.config.js
@@ -5,10 +5,12 @@ dotenv.config()
 
 //Creamos una constante de facil acceso a mis variables de entorno
 const ENVIRONMENT = {
-    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONECTION_STRING,
+    PORT: process.env.PORT,
+    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
     GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
-}
+    BACKEND_URL: process.env.BACKEND_URL
+};
 
 //prueba
 
