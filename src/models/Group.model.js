3193a930e6cc3bf6b054d3e94a39d2ed0e7ee1ad
Irene Magozzi <irenemagozzi@gmail.com>
2025-10-20 20:44:19 -0300

    Intento de arreglo de un import circular

--- src/models/Group.model.js
+++ src/models/Group.model.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-import GroupMember from "./GroupMember.model.js";
-import GroupMessage from "./GroupMessage.model.js";
-
 const groupSchema = new mongoose.Schema(
     {
         name: {
@@ -38,9 +35,12 @@ groupSchema.pre("findOneAndDelete", async function (next) {
     try {
         const groupId = this.getQuery()._id;
 
+        const GroupMember = (await import("./GroupMember.model.js")).default;
+        const GroupMessage = (await import("./GroupMessage.model.js")).default;
+
         await Promise.all([
             GroupMember.deleteMany({ group_id: groupId }),
-            GroupMessage.deleteMany({ group_id: groupId })
+            GroupMessage.deleteMany({ group_id: groupId }),
         ]);
 
         console.log(`Grupo ${groupId} eliminado junto con sus mensajes y miembros.`);
