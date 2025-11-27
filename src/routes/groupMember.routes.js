f343d9161be4dea48cdd294894c47e0688a17991
Irene Magozzi <irenemagozzi@gmail.com>
2025-11-26 23:41:17 -0300

    Arreglo de ruta

--- src/routes/groupMember.routes.js
+++ src/routes/groupMember.routes.js
@@ -10,7 +10,7 @@ groupMember_router.use(authMiddleware);
 
 groupMember_router.post("/", requireAdmin, GroupMemberController.addMember);
 groupMember_router.delete("/:group_id/:member_id", requireAdmin, GroupMemberController.removeMember);
-groupMember_router.get("/user/me", authMiddleware, GroupMemberController.getGroupsByUser);
+groupMember_router.get("/user/me", GroupMemberController.getGroupsByUser);
 groupMember_router.get("/:group_id", isGroupMemberMiddleware, GroupMemberController.getMembers);
 groupMember_router.put("/sync", requireAdmin, GroupMemberController.syncMembers);
 groupMember_router.patch("/:group_id/:member_id/role", requireAdmin, GroupMemberController.changeRole);
